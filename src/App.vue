732b2195c05ba60fb2ae011d83ae77f5ca6c6393
Felipe <feferreira.sf@gmail.com>
2023-06-24 15:44:25 -0300

    feat: footer section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import FooterSection from "./components/FooterSection.vue";
 import HeaderSection from "./components/HeaderSection.vue";
 </script>
 
 <template>
   <HeaderSection />
   <RouterView />
+  <FooterSection />
 </template>
---
+++ src/components/FooterSection.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import BottomNav from './BottomNav.vue';
+</script>
+
+<template>
+  <BottomNav />
+  <footer class="footer">
+    <h1 class="footer__title">UMA EMPRESA DO GRUPO</h1>
+    <img src="../assets/images/logo_footer.png" alt="" />
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  padding: 1rem;
+  background-color: var(--clr-gray-700);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+
+.footer__title {
+  font-size: 1.25rem;
+  font-weight: 500;
+  color: var(--clr-neutral);
+}
+</style>
