f05c12547d83fbc354863b7fb2af84c558aef90c
Felipe <feferreira.sf@gmail.com>
2023-06-24 17:29:53 -0300

    feat: product slider import

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import HeroBanner from '@/components/HeroBanner.vue';
-import ProductsCta from '@/components/ProductsCta.vue';
+import HeroBanner from "@/components/HeroBanner.vue";
+import ProductSlider from "@/components/ProductSlider.vue";
+import ProductsCta from "@/components/ProductsCta.vue";
 </script>
+
 <template>
   <main>
     <HeroBanner />
     <ProductsCta />
+    <ProductSlider />
   </main>
 </template>
-
