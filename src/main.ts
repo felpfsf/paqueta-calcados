529ce7036fb3abe3d98a9272a440f5dd420b43bf
Felipe <feferreira.sf@gmail.com>
2023-06-24 13:17:47 -0300

    feat: vue router configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './styles/styles.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import router from "./router";
+
+import "./styles/styles.css";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
