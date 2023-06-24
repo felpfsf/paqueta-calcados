b84bf6a283ff6121fc75be39aab72cdd7b5a251a
Felipe <feferreira.sf@gmail.com>
2023-06-24 15:26:48 -0300

    feat: added header

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import HeaderSection from "./components/HeaderSection.vue";
+</script>
+
 <template>
-  <main>
-    <div>
-      <h1>Hello h1</h1>
-      <h2>Hello h2</h2>
-      <h3>Hello h3</h3>
-      <h4>Hello h4</h4>
-      <h5>Hello h5</h5>
-      <h6>Hello h6</h6>
-      <p>Hello p</p>
-      <ol>
-        <li>ol1</li>
-        <li>ol2</li>
-        <li>ol3</li>
-      </ol>
-      <ul>
-        <li>ul1</li>
-        <li>ul2</li>
-        <li>ul3</li>
-      </ul>
-      <dl>
-        <dt>Coffee</dt>
-        <dd>Black hot drink</dd>
-        <dt>Milk</dt>
-        <dd>White cold drink</dd>
-      </dl>
-    </div>
-  </main>
+  <HeaderSection />
+  <RouterView />
 </template>
