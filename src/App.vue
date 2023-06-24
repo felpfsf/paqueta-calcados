6866c2ba429a783444e77e9ec913624225573344
Felipe <feferreira.sf@gmail.com>
2023-06-24 12:42:41 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div>
+      <h1>Hello h1</h1>
+      <h2>Hello h2</h2>
+      <h3>Hello h3</h3>
+      <h4>Hello h4</h4>
+      <h5>Hello h5</h5>
+      <h6>Hello h6</h6>
+      <p>Hello p</p>
+      <ol>
+        <li>ol1</li>
+        <li>ol2</li>
+        <li>ol3</li>
+      </ol>
+      <ul>
+        <li>ul1</li>
+        <li>ul2</li>
+        <li>ul3</li>
+      </ul>
+      <dl>
+        <dt>Coffee</dt>
+        <dd>Black hot drink</dd>
+        <dt>Milk</dt>
+        <dd>White cold drink</dd>
+      </dl>
+    </div>
+  </main>
+</template>
