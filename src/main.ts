cd16ff9b9b4415a4a555983eefdec3764806a9de
Felipe <feferreira.sf@gmail.com>
2023-06-26 20:57:19 -0300

    feat: init pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
+import { createPinia } from "pinia";
 import { createApp } from "vue";
-
 import App from "./App.vue";
 import router from "./router";
-
 import "./styles/styles.css";
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
