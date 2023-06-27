c863effe033feff28e74812df9064dd6ec076f2b
Felipe <feferreira.sf@gmail.com>
2023-06-27 13:08:23 -0300

    feat: function to add items to local storage

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,17 +1,23 @@
 import { ShoesProps } from "@/types/shoes.models";
 import { defineStore } from "pinia";
 
-interface CartState {
-  products: ShoesProps[];
+const CART_STORAGE_KEY = "@paqueta-store: shopping-cart-v1.0.0";
+
+interface CartItem extends ShoesProps {
   quantity: number;
 }
 
+const getCartFromLocalStorage = (): CartItem[] => {
+  const savedItems = localStorage.getItem(CART_STORAGE_KEY);
+  return savedItems ? JSON.parse(savedItems) : [];
+};
+
 export const useCartStore = defineStore("cart", {
   state: () => ({
-    products: [] as ShoesProps[],
+    products: getCartFromLocalStorage(),
   }),
   getters: {
-    items: (state): ShoesProps[] =>
+    items: (state): CartItem[] =>
       state.products.reduce((items, product) => {
         const existingProduct = items.find((it) => it.id === product.id);
 
@@ -22,20 +28,26 @@ export const useCartStore = defineStore("cart", {
         }
 
         return items;
-      }, [] as ShoesProps[]),
+      }, [] as CartItem[]),
   },
   actions: {
     addItem(product: ShoesProps) {
-      this.products.push(product);
+      const cartItem = { ...product, quantity: 1 };
+      this.products.push(cartItem);
+      this.saveItemstoLocalStorage()
       console.log("Produto adicionado ao carrinho", product);
-      console.log("Lista de productos no carrinho ->", this.items)
+      console.log("Lista de productos no carrinho ->", this.items);
     },
     removeItem(product: ShoesProps) {
       // this.products.filter((it) => it.id !== product.id);
-      const index = this.products.findIndex(item => item.id === product.id)
-      if(index !== -1) this.items.splice(index, 1);
+      const index = this.products.findIndex((item) => item.id === product.id);
+      if (index !== -1) this.items.splice(index, 1);
+      this.saveItemstoLocalStorage()
       console.log("Produto removido do carrinho", product);
-      console.log("Lista atualizada de produtos no carrinho ->", this.items)
+      console.log("Lista atualizada de produtos no carrinho ->", this.items);
+    },
+    saveItemstoLocalStorage() {
+      localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(this.items));
     },
   },
 });
