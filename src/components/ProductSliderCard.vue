baa5bd91594d529039f26a85a9d8751978699956
Felipe <feferreira.sf@gmail.com>
2023-06-27 16:20:22 -0300

    feat: condition to show msg if product is sold out

--- src/components/ProductSliderCard.vue
+++ src/components/ProductSliderCard.vue
@@ -18,28 +18,28 @@ export default {
   setup(props) {
     const product = props.product;
     const cartStore = useCartStore();
-    const favoriteStore = useFavoriteStore()
+    const favoriteStore = useFavoriteStore();
 
     const addProductToFavorite = () => {
-      favoriteStore.addToFavorites(product)
+      favoriteStore.addToFavorites(product);
     };
 
     const toggleFavorite = () => {
-      if(isFavorited.value){
-        favoriteStore.removeFromFavorites(product)
+      if (isFavorited.value) {
+        favoriteStore.removeFromFavorites(product);
       } else {
-        favoriteStore.addToFavorites(product)
+        favoriteStore.addToFavorites(product);
       }
-    }
-
-    const addItemToCart = () => {
-      cartStore.addItem(product)
     };
 
-    const isFavorited = computed(()=>{
-      const favoriteItem = favoriteStore.favoriteItems
-      return favoriteItem.some(item => item.id === product.id)
-    })
+    const addItemToCart = () => {
+      cartStore.addItem(product);
+    };
+
+    const isFavorited = computed(() => {
+      const favoriteItem = favoriteStore.favoriteItems;
+      return favoriteItem.some((item) => item.id === product.id);
+    });
 
     return { addItemToCart, addProductToFavorite, isFavorited, toggleFavorite };
   },
@@ -54,10 +54,20 @@ export default {
       <h2 class="text-xl product__value">R$ {{ product.price.value }}</h2>
       <h4 class="text-xs product__sub">OU 9X R$ 16,66</h4>
     </div>
-    <button class="button" @click="addItemToCart">Comprar</button>
-    <button class="favorite"  @click="toggleFavorite">
-      <Heart :class="{'favorite--active': isFavorited}" />
+    <button
+      class="button"
+      :class="{ 'sold--out': product.soldout, 'gradient--button': !product.soldout }"
+      :disabled="product.soldout"
+      @click="addItemToCart"
+    >
+      {{ product.soldout ? "Me Avise Quando Chegar" : "Comprar" }}
     </button>
+    <button class="favorite" @click="toggleFavorite">
+      <Heart :class="{ 'favorite--active': isFavorited }" />
+    </button>
+    <span v-if="product.soldout" class="is-soldout text-sm"
+      >Produto Esgotado</span
+    >
   </div>
 </template>
 
@@ -68,6 +78,7 @@ export default {
   padding: 1.5rem;
   border-radius: 0.375rem;
   box-shadow: 0px 4px 21px 0px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
 }
 .product__name {
   font-weight: 500;
@@ -89,16 +100,28 @@ export default {
 }
 .favorite {
   position: absolute;
-  top: 5%;
-  right: 10%;
+  top: 10%;
+  right: 8%;
 }
 .favorite:hover svg {
-  fill: red;
-  color: transparent;
-  transition: all 0.2s ease-in-out;
+  fill: var(--clr-danger);
+  color: var(--clr-danger);
 }
-.favorite--active{
-  fill: red;
-  color: transparent;
+.favorite--active {
+  fill: var(--clr-danger);
+  color: var(--clr-danger);
+}
+.is-soldout {
+  position: absolute;
+  top: 0;
+  left: 0;
+  text-align: center;
+  width: 100%;
+  background: var(--clr-accent);
+  color: var(--clr-neutral);
+  /* font-size: 14px; */
+  padding: 8px;
+  font-weight: 700;
+  text-transform: uppercase;
 }
 </style>
