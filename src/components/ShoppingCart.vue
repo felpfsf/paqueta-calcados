f5fe64697960b378bb73365608e5a9e75de7d8a0
Felipe <feferreira.sf@gmail.com>
2023-06-28 13:30:21 -0300

    feat: items in cart counter reactive

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,12 +1,26 @@
-<script setup lang="ts">
+<script lang="ts">
+import { useCartStore } from "@/stores/cartStore";
 import { ShoppingBag } from "lucide-vue-next";
+import { storeToRefs } from "pinia";
+export default {
+  name: "ShoppingCart",
+  setup() {
+    const cartStore = useCartStore()
+    const {items} = storeToRefs(cartStore)
+    console.log(items.value.length)
+    const itemsInCart = items.value.length
+
+    return { itemsInCart }
+  },
+  components: { ShoppingBag },
+};
 </script>
 
 <template>
   <div class="main__wrapper">
     <div class="icon__wrapper">
-      <ShoppingBag />
-      <span class="item__tooltip text-xs">1</span>
+      <ShoppingBag :class="{'cart--active': itemsInCart > 0}"/>
+      <span class="item__tooltip text-xs" v-if="itemsInCart > 0">{{itemsInCart}}</span>
     </div>
     <p class="label text-sm">Sacola</p>
   </div>
@@ -22,6 +36,10 @@ import { ShoppingBag } from "lucide-vue-next";
   position: relative;
   display: flex;
   align-items: center;
+  justify-content: center;
+}
+.cart--active{
+  color: var(--clr-accent);
 }
 .item__tooltip {
   position: absolute;
@@ -37,10 +55,8 @@ import { ShoppingBag } from "lucide-vue-next";
   justify-content: center;
   line-height: 0;
 }
-
 .label {
   line-height: 0;
-  font-weight:500;
+  font-weight: 500;
 }
-
 </style>
