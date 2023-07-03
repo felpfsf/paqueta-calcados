499079d03cb077be1d769228489a4fc8a76dbd31
Felipe <feferreira.sf@gmail.com>
2023-07-03 16:03:14 -0300

    style: background color changes

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -141,6 +141,7 @@ export default {
   max-width: 50%;
   width: 100%;
   background: var(--clr-neutral);
+  opacity: 0.2;
   z-index: 10;
   padding: 2rem 1rem;
   box-shadow: "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px";
